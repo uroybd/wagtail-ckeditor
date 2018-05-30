f8da0300533ed5ffaf29ba9da17f4a85357c1983
Utsob Roy <utsob@codesign.com.bd>
2018-05-30 13:10:20 +0600

    package fix [master]

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup(
-   name='wagtail-ckeditor',
-   version='0.9',
-   description='CKEditor widget for Wagtail CMS',
-   author='Martin Mastny',
-   author_email='martin.mastny@vscht.cz',
-   packages=['wagtail_ckeditor'],
-   install_requires=['wagtail'],
+    name='wagtail-ckeditor',
+    version='0.9',
+    description='CKEditor widget for Wagtail CMS',
+    author='Martin Mastny',
+    author_email='martin.mastny@vscht.cz',
+    packages=['wagtail_ckeditor'],
+    install_requires=['wagtail'],
+    include_package_data=True
 )
